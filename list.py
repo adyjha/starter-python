c3b12a2ab35b4817662b534c241dc936b98b7159
adyjha <adyansh.jha@gmail.com>
2020-04-07 12:00:36 +1000

    learning list variable

---
+++ list.py
@@ -0,0 +1,16 @@
+shoppingList=['Gummies','Chocolates','Toys','Tablet']
+print(shoppingList)
+quantity=['60','30','10','2']
+print(quantity)
+#print(shoppingList+quantity)
+print(shoppingList[3])
+shoppingList[1]='Hat'
+print(shoppingList)
+print(shoppingList[1:3])
+del shoppingList[1]
+print(shoppingList)
+del (shoppingList[2])
+print(shoppingList)
+shoppingList.append('Phone')
+shoppingList.append('Puppy')
+print(shoppingList)
